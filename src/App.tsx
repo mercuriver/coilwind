dbc8a2ae35c4b246a61ba35033667b139a50ed3a
mercuriver <mercuriver@gmail.com>
2021-06-25 23:49:47 +0900

    chore: install babel-loader

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div>Wello, Horld!</div>
+      <h1>{JSON.stringify(process.env)}</h1>
+    </>
+  );
+};
+
+export default App;
