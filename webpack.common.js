5683abe6fc98d8f33d63121de00d982c51db55f7
mercuriver <mercuriver@gmail.com>
2021-06-26 19:13:02 +0900

    fix: tsconfig paths alias issue

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
   },
 
   resolve: {
-    extensions: [".js", ".jsx", ".ts", "tsx"],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
     alias: {
       "@": path.resolve(__dirname, "src/"),
     },
